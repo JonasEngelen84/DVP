637271b8ef8e437a80257e3f694568167bbfe65c
Jonas Engelen <jonasEngelen@hotmail.de>
2023-10-12 08:18:39 +0200

    Klasse Clothes hinzugefügt

---
+++ Models/Clothes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVP.Models
+{
+    internal class Clothes
+    {
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Season { get; set; }
+        public string Comment { get; set; }
+        public int Count { get; set; }
+        public int Prize { get; set; }
+
+        public Clothes() { }
+    }
+}
