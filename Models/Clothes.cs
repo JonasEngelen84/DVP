7487e423d5c6663c0cd5ab03e2b4aa3051c9c737
Jonas Engelen <jonasEngelen@hotmail.de>
2023-10-22 17:44:33 +0200

    - EmployeesDetailsDataGrid - GridSplitter

--- Models/Clothes.cs
+++ Models/Clothes.cs
@@ -6,15 +6,22 @@ using System.Threading.Tasks;
 
 namespace DVP.Models
 {
-    internal class Clothes
+    internal abstract class Clothes
     {
         public string Name { get; set; }
         public string Size { get; set; }
         public string Season { get; set; }
-        public string Comment { get; set; }
         public int Count { get; set; }
         public int Prize { get; set; }
+        public string Comment { get; set; }
 
-        public Clothes() { }
+        //public Clothes(string name, string size, string season, int count, int prize)
+        //{
+        //    Name = name;
+        //    Size = size;
+        //    Season = season;
+        //    Count = count;
+        //    Prize = prize;
+        //}
     }
 }
