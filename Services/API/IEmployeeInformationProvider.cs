a7a87e8673297ac2f6473ff4aa3f7f16f64ffe52
Jonas Engelen <jonasEngelen@hotmail.de>
2023-10-26 17:23:32 +0200

    - Interfaces - ViewModelBase

---
+++ Services/API/IEmployeeInformationProvider.cs
@@ -0,0 +1,14 @@
+﻿using DVP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVP.Services.API
+{
+    public interface IEmployeeInformationProvider
+    {
+        List<Employee> EmployeesList { get; set; }
+    }
+}
